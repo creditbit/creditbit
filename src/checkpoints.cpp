a4f7ff17a7f595ffb1311ec2a497bb1b84b00490
creditbit <support@creditbit.org>
2015-11-06 12:17:04 +0100

    Update checkpoints.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -31,10 +31,6 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpointsTestnet =
         boost::assign::map_list_of
         ( 0, hashGenesisBlockTestNet )
-        ( 1000, uint256("0x000000000041678b00eac05be1946a5473c9016db5c6a5ca437528ed199ae64c"))
-        ( 2977, uint256("0x00000000005e606b6a8b3f669091043042ff5e0d15084246bc217041cc0f5097"))
-        ( 5666, uint256("0x00000000002047da650012a9d980ecfcf5da528caa97ac88a5fcffc7b20e7db4"))
-        ( 8133, uint256("0x00000000002219ac6cc1ec2320e68bb4448c87439d86bfc3f6cbd06020bc8a87"))
         ;
 
     bool CheckHardened(int nHeight, const uint256& hash)
